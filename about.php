0e5bfb7c5140fc29d6aab9b0a91dbca82275d7ce
odada <eehd80yul@gmail.com>
2023-09-01 10:13:03 +0900

    first commit

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+include_once('./_common.php');
+
+// 페이지 제목 (웹접근성 관련)
+$g5['title'] = '단체소개';
+
+add_stylesheet('<link rel="stylesheet" href="'.G5_CSS_URL.'/sub.css">', 0);
+include_once(G5_PATH.'/head.php');
+?>
+
+
+
+about
+
+<?php
+include_once(G5_PATH.'/tail.php');
