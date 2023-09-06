8f1b8c3472f02b72f4bda10e4b214ce885fa3cac
odada <eehd80yul@gmail.com>
2023-09-06 15:44:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -19,49 +19,50 @@ include_once(G5_PATH.'/head.php');
 
 <!-- 본인 코드 삽입 -->
 <div class="mVisual">
-    <p class="txt">Onnuri Traditional Music Artists <br class="m"> Cooperative</p>
-</div>
-<!-- 단체소개 -->
-<div class="sec-g bg-red sec-about">
-    <div class="container">
-        <h2 class="sec-tit">단체소개 <em>About us</em> </h2>
+      <p class="txt">
+        Onnuri Traditional Music Artists <br class="m" />
+        Cooperative
+      </p>
+    </div>
+    <!-- 단체소개 -->
+    <div class="sec-g bg-red sec-about">
+      <div class="container">
+        <h2 class="sec-tit">단체소개 <em>About us</em></h2>
         <div class="sec-cont">
-            <p class="txt-intro">
-            온누리국악예술인협동조합은 경상북도 청도에 자리 잡고 있는 가족공동체로 구성된 전문 국악 연주 단체입니다.  <br class="pc">
-            협동조합 단원들은 다양한 레퍼토리 작품과 고도의 예술적 기능을 가진 연주자들로 국내외에서 수많은 기획, 초청공연을 하고 있습니다. <br class="pc">
+          <p class="txt-intro">
+            온누리국악예술인협동조합은 경상북도 청도에 자리 잡고 있는 가족공동체로 구성된 전문 국악 연주 단체입니다. <br class="pc" />
+            협동조합 단원들은 다양한 레퍼토리 작품과 고도의 예술적 기능을 가진 연주자들로 국내외에서 수많은 기획, 초청공연을 하고 있습니다. <br class="pc" />
             강력한 응집력과 자유분방한 앙상블로 이루어지는 크로스 오브 연주는 전통과 동시대성의 조화를 절묘하게 표현하며 미래의 한국 문화를 창조해 가고 있습니다.
-            </p>
-            <a href="#" class="btn-w">자세히 보기</a>
+          </p>
+          <a href="#" class="btn-w">자세히 보기</a>
         </div>
+      </div>
     </div>
-</div>
-<!-- 단체활동 -->
-<div class="sec-g sec-facility bg-gray">
-    <div class="container">
-        <h2 class="sec-tit">단체활동 <em>What We Do</em> </h2>
+    <!-- 단체활동 -->
+    <div class="sec-g sec-facility bg-gray">
+      <div class="container">
+        <h2 class="sec-tit">단체활동 <em>What We Do</em></h2>
         <div class="sec-cont">
-            <img src="images/img-doing.jpg" alt="">
-            <p class="txt-intro">
-            온누리 국악예술인 협동조합은 공연활동, 교육 활동(어린이 국악 클래스, 어르신 평생교육), 체험활동(국악기 체험, 미니어처 제작 체험, <br class="pc">
-            전통 놀이) 및 다양한 지역 사회 공헌 활동 등 다양한 사업들을 통하여 일자리 창출과 전통예술에 대한 대중들이 관심 증대를 위해 <br class="pc">
+          <img src="./images/img-doing.jpg" alt="" />
+          <p class="txt-intro">
+            온누리 국악예술인 협동조합은 공연활동, 교육 활동(어린이 국악 클래스, 어르신 평생교육), 체험활동(국악기 체험, 미니어처 제작 체험, <br class="pc" />
+            전통 놀이) 및 다양한 지역 사회 공헌 활동 등 다양한 사업들을 통하여 일자리 창출과 전통예술에 대한 대중들이 관심 증대를 위해 <br class="pc" />
             노력하고 있습니다.
-            </p>
-            <a href="#" class="btn-w">자세히 보기</a>
+          </p>
+          <a href="#" class="btn-w">자세히 보기</a>
         </div>
+      </div>
     </div>
+
+<div class="latest_top_wr">
+    <?php
+    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
+    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
+    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+	echo latest('pic_list', 'notice', 4, 23);		// 최소설치시 자동생성되는 공지사항게시판
+    ?>
 </div>
-
-<h2 class="sound_only">최신글</h2>
-
-
-<div class="sec-g latest_wr">
-    <div class="container">
-        <div>
-            <?php echo latest("basic", "contact", 5, 25); ?>
-        </div>        
-    </div>
-</div>
-
 <!-- //본인 코드 삽입 -->
+
 <?php
 include_once(G5_PATH.'/tail.php');
